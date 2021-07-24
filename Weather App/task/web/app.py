c99e586617f04fb26f4c6d5f002bc90f6a5c8a23
Vladimir Rujević <vladimir.rujevic@live.com>
2021-07-24 18:26:56 +0200

    stage 5 finished project

--- Weather App/task/web/app.py
+++ Weather App/task/web/app.py
@@ -1,12 +1,13 @@
 import sys
 
 import requests
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
-api_key = ''
+app.secret_key = b'thisisasecretkey'
+api_key = 'c6f6f944a69c53ff9cc603108d332bfa'
 db = SQLAlchemy(app)
 
 
@@ -15,28 +16,45 @@ class Weather(db.Model):
     name = db.Column(db.String(80), unique=True, nullable=False)
 
 
-
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'GET':
-        list = []
+        cities = []
         rows = Weather.query.all()
         for row in rows:
             r = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={row.name}&appid={api_key}&units=metric')
             r = r.json()
-            list.append({
+            cities.append({
                 'name': r['name'],
                 'state': r['weather'][0]['main'],
                 'temperature': r['main']['temp'],
                 'id': row.id
             })
-        return render_template('index.html', cities=list)
+        return render_template('index.html', cities=cities)
     elif request.method == 'POST':
         city = request.form['city_name']
-        entry = Weather(name=city)
-        db.session.add(entry)
-        db.session.commit()
+        r = requests.get(f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric')
+        r = r.json()
+        if 'cod' in r and r['cod'] == '404':
+            flash("The city doesn't exist!")
+        else:
+            e = Weather.query.filter_by(name=city).first()
+            if e is None:
+                entry = Weather(name=city)
+                db.session.add(entry)
+                db.session.commit()
+            else:
+                flash("The city has already been added to the list!")
         return redirect(url_for('index'))
+
+
+@app.route('/delete', methods=['POST'])
+def delete():
+    city_id = request.form['id']
+    city = Weather.query.filter_by(id=city_id).first()
+    db.session.delete(city)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 
 # don't change the following way to run flask:
